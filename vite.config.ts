8dc85db2950e872f9240d92246734a928988c008
zaidunz <zid4nsyhid@gmail.com>
2025-10-24 10:55:30 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/zidan-booking-futsal/', 
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
